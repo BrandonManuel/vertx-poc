9a6b1fd935e313c5545765ef4a1c52b2d090c84f
Sam Isang <sisang3@github.com>
2019-10-08 13:59:58 -0400

    testIndexHandler issues

--- src/test/java/com/homedepot/hackathon/gcpreauth/vertx_gcpreauth/TestMainVerticle.java
+++ src/test/java/com/homedepot/hackathon/gcpreauth/vertx_gcpreauth/TestMainVerticle.java
@@ -7,9 +7,25 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import io.vertx.ext.web.RoutingContext;
+
+import io.vertx.ext.unit.TestSuite;
+import io.vertx.ext.unit.Async;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientRequest;
+
+import com.homedepot.hackathon.gcpreauth.vertx_gcpreauth.HttpVerticle;
+import java.net.ServerSocket;
+import java.lang.reflect.Method;
+
+
+
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
+
+  private Integer port;
+  Vertx vertx;
 
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
@@ -19,5 +35,28 @@ public class TestMainVerticle {
   @Test
   void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
     testContext.completeNow();
+  }
+
+  @Test
+  void testIndexhandler(VertxTestContext testContext) {
+    TestSuite suite = TestSuite.create("test_suite");
+    suite.test("test_suite", context -> {
+
+      Async async1 = context.async();
+      HttpClient client = vertx.createHttpClient();
+      HttpClientRequest req = client.get(8080, "localhost", "/");
+      req.exceptionHandler(err -> context.fail(err.getMessage()));
+      req.handler(resp -> {
+        context.assertEquals(200, resp.statusCode());
+        async1.complete();
+      });
+      req.end();
+
+      Async async2 = context.async();
+      vertx.eventBus().consumer("the-address", msg -> {
+        async2.complete();
+      });
+    });
+    suite.run();
   }
 }
